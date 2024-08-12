77f7c95645cafdf3dddf86f93686b51c78659ab0
Likhi17 <likhithlikhi17@gmail.com>
2024-08-12 20:35:49 +0530

    All the files for the 10th Aug 2024 are added

---
+++ src/ex_10082024/Lab009.py
@@ -0,0 +1,10 @@
+# Dynamically typed
+# At the run time, data of the variable can be changed. and you don't need to
+# mention the data type - Python - Int is very smart
+
+age = 45
+print(age)
+print(type(age))
+age = "Likhith"
+print(age)
+print(type(age))
