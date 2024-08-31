92aa00579534114d030f4c6611bf5eba3a229edf
Likhi17 <likhithlikhi17@gmail.com>
2024-08-31 21:37:13 +0530

    All the files for the 31st Aug 2024 are added

---
+++ src/ex_31082024/Lab097.py
@@ -0,0 +1,55 @@
+# Constructor
+# Special Function in Class,  __init__()
+# It will be automatically called when you create an Object
+
+class Employee:
+    name = None
+    age = None
+    phone = None
+    address = None
+    eid = None
+
+    def __init__(self, name, age, phone, address, eid):
+        self.name = name
+        self.age = age
+        self.phone = phone
+        self.address = address
+        self.eid = eid
+
+    def Walk(self):
+        print(f"{self.name} is Walking")
+        return None
+
+    def Talk(self):
+        print("Talking")
+        print(f"{self.name} is Talking ")
+        return None
+
+    def Employee_Details(self):
+        print("----------Employee_Details---------")
+        print(
+            f" User details are :\n Name = {self.name} \n age = {self.age} \n phone = {self.phone} \n address = {self.address} \n Eid = {self.eid} ")
+        return None
+
+
+print("Enter details of employee 1")
+name1 = input("Enter the name of employee \n")
+age1 =  int(input("Enter the age of employee \n"))
+phone1 = int(input("Enter the phone number of employee \n"))
+address1 = input("Enter the address of employee \n")
+eid1 = int(input("Enter the employee_id of employee \n"))
+
+print("Enter details of employee 2")
+name2 = input("Enter the name of employee \n")
+age2 =  int(input("Enter the age of employee \n"))
+phone2 = int(input("Enter the phone number of employee \n"))
+address2 = input("Enter the address of employee \n")
+eid2 = int(input("Enter the employee_id of employee \n"))
+
+Employee1=Employee(name1,age1,phone1,address1,eid1)
+Employee1.Walk()
+Employee1.Employee_Details()
+
+Employee2 = Employee(name2,age2,phone2,address2,eid2)
+Employee2.Talk()  # ->
+Employee2.Employee_Details()
